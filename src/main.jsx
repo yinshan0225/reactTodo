52a0ce8c605c58f6807223afbe9419614dbd475e
yinshan0225 <k4564123@gmail.com>
2025-05-16 23:31:28 +0800

    20250516

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './App.css'  // CSS 就這樣引入
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
